298d6493efa67b7f587bdaa170429f1535224b5f
Maheboob Sab <mahiboobbadeghar6@gmail.com>
2024-06-07 09:43:31 +0530

    first commit

--- practice/practice.js
+++
@@ -1,9 +0,0 @@
-const h1 = document.querySelector("body h1");
-
-const arr = ["Aalim", "Asif", "Fazal", "Vikram"];
-let currentIndex = 0;
-console.log(arr.length)
-h1.addEventListener("click", () => {
-    h1.textContent = arr[currentIndex];
-    currentIndex = (currentIndex + 1) % arr.length;
-});
